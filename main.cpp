8e2e9220646b0f89c27ced4ba814e52328bdb447
jermiane1 <jamesamalias@gmail.com>
2025-09-21 21:42:38 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "   TTTTTTTTTTTTTT\n";
+    std::cout << "         TT  get       OO \n";
+    std::cout << "         TT   well    O  O\n";
+    std::cout << "         TT     soon   OO \n";
+    std::cout << "         TT  RRRRRRR  IIII\n";
+    std::cout << "         TT  RR   RR   II \n";
+    std::cout << "         TT  RR  RRR   II \n";
+    std::cout << "         TT  RR RR     II \n";
+    std::cout << "         TT  RR  RR    II \n";
+    std::cout << "         TT  RR   RR  IIII\n";
+    
+}
